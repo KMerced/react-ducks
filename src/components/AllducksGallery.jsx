11edf3dd94d74b3f9d09c06321ee8e700bb28f13
KMerced <kaylanyrmerced@proton.me>
2025-11-29 19:25:50 -0500

    Made some changes to the allducks gallery so that it will pull the list of ducks from the server instead of it all being hardcoded. This way, when a new duck is added, all you have to do it refresh the page and the newly added duck will show up at the very end!

--- src/components/AllducksGallery.jsx
+++ src/components/AllducksGallery.jsx
@@ -1,53 +1,23 @@
 import "./../css/AllducksGallery.css";
 import {Link} from "react-router-dom";
 import {useState, useEffect} from "react";
-import batman from "../css/images/ducks/batman.jpg";
-import beige_easter from "../css/images/ducks/beige_easter.jpg";
-import blue from "../css/images/ducks/blue.jpg";
-import butterfly from "../css/images/ducks/butterfly.jpg";
-import construction_worker from "../css/images/ducks/construction_worker.jpg";
-import fairy from "../css/images/ducks/fairy.jpg";
-import firefly from "../css/images/ducks/firefly.jpg";
-import football from "../css/images/ducks/football.jpg";
-import frankie_green_glitter from "../css/images/ducks/frankie_green_glitter.jpg";
-import mini_dovakiin from "../css/images/ducks/mini_dovakiin.jpg";
-import munchkin_bath from "../css/images/ducks/munchkin_bath.jpg";
-import outlaw from "../css/images/ducks/outlaw.jpg";
-import pink_glitter_witch from "../css/images/ducks/pink_glitter_witch.jpg";
-import pumpkin from "../css/images/ducks/pumpkin.jpg";
-import purple_with_green_bill from "../css/images/ducks/purple_with_green_bill.jpg";
-import scout from "../css/images/ducks/scout.jpg";
-import skeleton from "../css/images/ducks/skeleton.jpg";
-import witch from "../css/images/ducks/witch.jpg";
-import zombie from "../css/images/ducks/zombie.jpg";
+import axios from "axios";
 import AddDuck from "./AddDuck";
 
-const ducks = [
-    {name: "Batman Duck", img: batman, link: "/Duckpage"},
-    {name: "Beige Easter Duck", img: beige_easter, link: "/Duckpage"},
-    {name: "Blue Duck", img: blue, link: "/Duckpage"},
-    {name: "Butterfly Duck", img: butterfly, link: "/Duckpage"},
-    {name: "Construction Worker Duck", img: construction_worker, link: "/Duckpage"},
-    {name: "Fairy Duck", img: fairy, link: "/Duckpage"},
-    {name: "Firefly Duck", img: firefly, link: "/Duckpage"},
-    {name: "Football Duck", img: football, link: "/Duckpage"},
-    {name: "Frankie's Fun Park Green Glitter Duck", img: frankie_green_glitter, link: "/Duckpage"},
-    {name: "Mini Dovakiin Duck", img: mini_dovakiin, link: "/Duckpage"},
-    {name: "Munchkin's Bath Duck", img: munchkin_bath, link: "/Duckpage"},
-    {name: "Outlaw Duck", img: outlaw, link: "/Duckpage"},
-    {name: "Pink Glitter Witch Duck", img: pink_glitter_witch, link: "/Duckpage"},
-    {name: "Pumpkin Duck", img: pumpkin, link: "/Duckpage"},
-    {name: "Purple With Green Bill Duck", img: purple_with_green_bill, link: "/Duckpage"},
-    {name: "Scout Duck", img: scout, link: "/Duckpage"},
-    {name: "Skeleton Duck", img: skeleton, link: "/Duckpage"},
-    {name: "Witch Duck", img: witch, link: "/Duckpage"},
-    {name: "Zombie Duck", img: zombie, link: "/Duckpage"}
-]
+const AllducksGallery = (props) => {
 
-const AllducksGallery = () => {
-
-    const [setDuck] = useState([]);
     const [showAddDialog, setShowAddDialog] = useState(false);
+    const [ducks,setDucks] = useState([]);
+
+    useEffect(()=> {
+    const loadDucks = async() => {
+        const response = await axios.get("https://ducks-server.onrender.com/api/ducks/");
+
+        setDucks(response.data.slice(0,props.num));
+    };
+
+    loadDucks();
+},[])
     
     const openAddDialog = () => {
         setShowAddDialog(true);
@@ -57,7 +27,7 @@ const AllducksGallery = () => {
     }
     const updateDucks = (duck) => {
         //Give a list, break up the list, then add to the list
-        setDuck((ducks)=>[...ducks,duck]);
+        setDucks((ducks)=>[...ducks,duck]);
     }
 
     return (
@@ -68,11 +38,11 @@ const AllducksGallery = () => {
                                 updateDucks/> ): ("")}
 
             <main id="all-ducks-main" className="columns">
-                {ducks.map((ducks) => (
+                {ducks.map((duck) => (
                     <section className="all-ducks">
-                        <h3>{ducks.name}</h3>
-                        <img src={ducks.img}/>
-                        <Link to={ducks.link}>{ducks.name}</Link>
+                        <h3>{duck.name}</h3>
+                        <img src={`https://ducks-server.onrender.com/${duck.img}`}/>
+                        <Link to="/Duckpage" state={{duck}}>{duck.name}</Link>
                     </section>
                 ))}
             </main>
--- src/components/Duckimage.jsx
+++ src/components/Duckimage.jsx
@@ -2,21 +2,20 @@ import {useState, useEffect} from "react";
 import axios from "axios";
 import "./../css/Duckinfo.css";
 
-const Duckimage = ({duck}) => {
+const Duckimage = ({}) => {
     const [ducks, setDucks] = useState([]);
 
     useEffect(()=>{
         const loadDucks = async() => {
-            const response = await axios.get("https://ducks-server.onrender.com/api/ducks");
+            const response = await axios.get("https://ducks-server.onrender.com/api/ducks/");
             setDucks(response.data[0]);
         };
 
         loadDucks();
     },[]);
-
     return (
             <div id="image">
-                <img id="img" src={"https://ducks-server.onrender.com/images/"+ducks.img}/>
+                <img id="img" src={`https://ducks-server.onrender.com/${ducks.img}`}/>
             </div>
     )
 };
